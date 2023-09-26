48f552497b0d42dd13f311aa53bc984955e0852a
Euvickson <victor.eugennio@gmail.com>
2023-09-26 12:53:04 -0300

    Initial commit

---
+++ app/src/main/java/br/com/euvickson/movieapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package br.com.euvickson.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.euvickson.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        content()
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface (color = MaterialTheme.colorScheme.background) {
+        Text(text = "Hello")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaltPreview() {
+    MyApp {
+        MainContent()
+    }
+}
