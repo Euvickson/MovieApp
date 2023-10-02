4b8179a040f2c0fe9bf879e64f5658312a6ec891
Euvickson <victor.eugennio@gmail.com>
2023-10-02 13:00:10 -0300

    Creating the button to popBackStack on the detail Screen, so we can go back to the HomeScreen

--- app/src/main/java/br/com/euvickson/movieapp/screens/detail/DetailScreen.kt
+++ app/src/main/java/br/com/euvickson/movieapp/screens/detail/DetailScreen.kt
@@ -2,14 +2,18 @@ package br.com.euvickson.movieapp.screens.detail
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -18,7 +22,16 @@ fun DetailScreen(navController: NavController, movieData: String?) {
         .fillMaxHeight()
         .fillMaxWidth()) {
         Column (horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+
             Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
+
+            Spacer(modifier = Modifier.height(23.dp))
+
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
