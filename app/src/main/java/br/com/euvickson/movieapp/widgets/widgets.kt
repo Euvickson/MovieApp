91905d11d7d94b18e564bd8327bbe986901625ed
Euvickson <victor.eugennio@gmail.com>
2023-10-02 13:52:21 -0300

    Refactoring the MovieRow (Card) Widget to show the director, and the Year of movie release.
    TODO: Use the Coil Library to show the Movie Image

--- app/src/main/java/br/com/euvickson/movieapp/widgets/widgets.kt
+++ app/src/main/java/br/com/euvickson/movieapp/widgets/widgets.kt
@@ -2,6 +2,7 @@ package br.com.euvickson.movieapp.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,12 +23,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.movieapp.model.Movie
+import br.com.euvickson.movieapp.model.getMovies
 
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit = {}) {
+fun MovieRow(movie: Movie = getMovies()[0], onItemClicked: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -53,8 +58,12 @@ fun MovieRow(movie: Movie, onItemClicked: (String) -> Unit = {}) {
             ) {
                 Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
             }
+            Column (modifier = Modifier.padding(4.dp)){
+                Text(text = movie.title, style = MaterialTheme.typography.bodyMedium)
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelSmall)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.labelSmall)
+            }
 
-            Text(text = movie.title)
         }
     }
 }
