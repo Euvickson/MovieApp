49b5a2eba299338ef52615eea88c30000966074a
Euvickson <victor.eugennio@gmail.com>
2023-10-03 13:36:51 -0300

    Added some information to the Homescreen movie Card. Added an animation to show the plot, the actors and the rating.
    TODO: Work on the detail screen, to get the movie from the id

--- app/src/main/java/br/com/euvickson/movieapp/widgets/widgets.kt
+++ app/src/main/java/br/com/euvickson/movieapp/widgets/widgets.kt
@@ -1,48 +1,57 @@
 package br.com.euvickson.movieapp.widgets
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.euvickson.movieapp.model.Movie
 import br.com.euvickson.movieapp.model.getMovies
 import coil.compose.AsyncImage
-import coil.transform.CircleCropTransformation
 
 
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClicked: (String) -> Unit = {}) {
+    var expanded by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable { onItemClicked(movie.id) },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = CardDefaults.cardElevation(6.dp),
@@ -66,6 +75,37 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClicked: (String) -> Unit = {}
                 Text(text = movie.title, style = MaterialTheme.typography.bodyMedium)
                 Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelSmall)
                 Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.labelSmall)
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+
+                        Text(buildAnnotatedString {
+
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Light)) {
+                                append(movie.plot)
+                            }
+
+                        }, modifier = Modifier.padding(6.dp))
+
+                        Divider(modifier = Modifier.padding(3.dp))
+
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.labelSmall)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.labelSmall)
+                    }
+                }
+
+                Icon(
+                    imageVector = if(expanded) {Icons.Filled.KeyboardArrowUp} else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable { expanded = !expanded },
+                    tint = Color.DarkGray
+                )
             }
 
         }
