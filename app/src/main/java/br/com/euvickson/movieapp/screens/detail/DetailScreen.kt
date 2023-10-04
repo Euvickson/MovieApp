d8161dfe96f596455a558dadbfc541a1e84c6807
Euvickson <victor.eugennio@gmail.com>
2023-10-04 12:58:27 -0300

    Filled the DetailScreen with information about the movie and Created a section in the Detail Screen to show a row with images

--- app/src/main/java/br/com/euvickson/movieapp/screens/detail/DetailScreen.kt
+++ app/src/main/java/br/com/euvickson/movieapp/screens/detail/DetailScreen.kt
@@ -1,23 +1,27 @@
 package br.com.euvickson.movieapp.screens.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -27,10 +31,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.euvickson.movieapp.model.Movie
+import br.com.euvickson.movieapp.model.getMovies
+import br.com.euvickson.movieapp.widgets.MovieRow
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(navController: NavController, movieData: String?) {
+fun DetailScreen(navController: NavController, movieId: String?) {
+
+    val movie = getMovies().filter { movie ->
+        movie.id == movieId
+    }.first()
+
     Column {
         TopAppBar(
             title = { Text(text = "Movies") },
@@ -41,7 +54,8 @@ fun DetailScreen(navController: NavController, movieData: String?) {
                 ),
             navigationIcon = {
                 Row {
-                    Icon(imageVector = Icons.Default.ArrowBack,
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
                         contentDescription = "Arrow Back",
                         modifier = Modifier
                             .size(30.dp)
@@ -53,14 +67,39 @@ fun DetailScreen(navController: NavController, movieData: String?) {
                 }
             }
         )
-            Column(modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
 
-                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineLarge)
-            }
+            MovieRow(movie = movie)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider(modifier = Modifier.padding(8.dp), color = Color.LightGray)
+
+            Text(text = "Movie Images")
+
+            HorizontalScrollableImageView(movie)
+
+        }
     }
+}
 
-
+@Composable
+private fun HorizontalScrollableImageView(movie: Movie) {
+    LazyRow {
+        items(movie.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp), elevation = CardDefaults.cardElevation(5.dp)
+            ) {
+                Column(Modifier.fillMaxSize().background(color = Color.White), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+                    AsyncImage(model = image, contentDescription = "Movie Poster")
+                }
+            }
+        }
+    }
 }
